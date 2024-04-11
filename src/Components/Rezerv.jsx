d7d7826b2e74ecfc07a7f4010b37fa949f0edf5c
asimmakhmudov <asimmahmudov18@gmail.com>
2024-04-11 20:54:59 +0400

    updates for Emil

--- src/Components/Rezerv.jsx
+++ src/Components/Rezerv.jsx
@@ -1,18 +1,48 @@
-import React from 'react'
-import { useState } from 'react'
-import RezervBG from './RezervBG'
+import React from "react";
+import { useState } from "react";
+import RezervBG from "./RezervBG";
 function Rezerv() {
-    const [dates, setDates] = useState([])
+  const [dates, setDates] = useState([]);
   return (
-    <div className='h-full relative' >
-    <div className='pt-[30px] pl-[60px]'>
-      <h1 className='font-bold text-[36px] text-[#023047]'>Reserv Təqvimi</h1>
-    <div>
-        {(dates.length > 0) ? dates.map( item => {`<div></div>`}) : <RezervBG/>}
-    </div>
-    </div>
-    </div>
-  )
+    <>
+      {/* <div className="h-full relative">
+        <div className="pt-[30px] pl-[60px]">
+          <h1 className="font-bold text-[36px] text-[#023047]">
+            Reserv Təqvimi
+          </h1>
+          <div>
+            {dates.length > 0 ? (
+              dates.map((item) => {
+                `<div></div>`;
+              })
+            ) : (
+              <RezervBG />
+            )}
+          </div>
+        </div>
+      </div> */}
+
+      {/* Aşağıdakını yuxarıdakı ilə müqayisə edərsən */}
+
+      {/* aşağıda pl və pt istifadə etmisən flex box öyrən hər dəfə margin, absolute, padding istifadə etmə, dəyişiklik edirəm sadecə resev hissəsini düzəldirəm digərlərini özün edərsən
+          padding verirsənsə çalış böyük ekranlar üçün hər tərəfə uyuğun şəkildə payla sağa verib sola verməməzlik eləmə edirsənsə marginlə et
+      */}
+      <div className="flex flex-col items-center justify-center min:h-[100vh] h-[100%] w-[100%] bg-[#EDEFFD]">
+        <div className="flex flex-col justify-start sm:items-start items-center h-[90%] sm:w-[80%] w-[95%] overflow-y-auto bg-[#FFF] rounded-[12px]">
+          <h1 className="font-bold sm:text-[36px] text-[24px] text-[#023047] mt-[5vh] sm:ml-[5vh]">Reserv Təqvimi</h1>
+          <div className="flex flex-col justify-center items-center w-[100%]">
+            {dates.length > 0 ? (
+              dates.map((item) => {
+                `<div></div>`;
+              })
+            ) : (
+              <RezervBG />
+            )}
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default Rezerv
+export default Rezerv;
--- src/Components/RezervBG.jsx
+++ src/Components/RezervBG.jsx
@@ -2,15 +2,39 @@ import React from "react";
 import ScHP from "./../Components/schedule.png";
 function RezervBG() {
   return (
-    <div>
-      <div className="absolute left-[50%] translate-x-[-50%] leading-10 text-[#023047] columns-1 pt-[60px] text-center">
-        <h2  className=" font-bold text-[24px]">Hazırda reserv edilən avtomobil yoxdur</h2>
-        <p className=" font-semibold text-[16px]">Sizin avtomobillər reserv edildikdə burada qeyd olunacaqdır</p>
+    <>
+      {/* <div>
+        <div className="absolute left-[50%] translate-x-[-50%] leading-10 text-[#023047] columns-1 pt-[60px] text-center">
+          <h2 className=" font-bold text-[24px]">
+            Hazırda reserv edilən avtomobil yoxdur
+          </h2>
+          <p className=" font-semibold text-[16px]">
+            Sizin avtomobillər reserv edildikdə burada qeyd olunacaqdır
+          </p>
+        </div>
+        <div className="absolute bottom-0 left-[50%] translate-x-[-50%]">
+          <img src={ScHP} />
+        </div>
+      </div> */}
+
+      {/* Aşağıdakını yuxarıdakı ilə müqayisə edib digər hissələrində də düzəliş edərsən */}
+
+      <div className="flex flex-col justify-center items-center mt-[5vh] sm:w-[100%] w-[90%]">
+        <div className="text-[#023047] text-center">
+          <h2 className="font-bold sm:text-[24px] text-[16px]">
+            Hazırda reserv edilən avtomobil yoxdur
+          </h2>
+          <p className="font-semibold sm:text-[12px] text-[10px] mt-[5px]">
+            Sizin avtomobillər reserv edildikdə burada qeyd olunacaqdır
+          </p>
+        </div>
+        <img
+          src={ScHP}
+          className="sm:h-[60vh] sm:w-[60vw] h-[100%] w-[95%] object-contain mt-[10px]"
+          alt=""
+        />
       </div>
-      <div className="absolute bottom-0 left-[50%] translate-x-[-50%]">
-        <img src={ScHP} />
-      </div>
-    </div>
+    </>
   );
 }
 
